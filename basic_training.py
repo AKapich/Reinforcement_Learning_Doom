48f5e5245032684bc3971090af2a65b06f617f8e
Aleks Kapich <aleks.kapich@gmail.com>
2024-04-19 19:37:07 +0200

    Small training adjustment

--- basic_training.py
+++ basic_training.py
@@ -13,10 +13,10 @@ callback = TrainAndLoggingCallback(check_freq=5000, save_path=CHECKPOINT_DIR)
 # train
 env = Env(render=False, scenario='basic')
 model = PPO("CnnPolicy", env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=0.0001, n_steps=2048)
-model.learn(total_timesteps=80000, callback=callback)
+model.learn(total_timesteps=100000, callback=callback)
 
 # test
-model = PPO.load(CHECKPOINT_DIR + '/best_model_80000', env=env)
+model = PPO.load(CHECKPOINT_DIR + '/best_model_100000', env=env)
 mean_reward, std_reward = evaluate_policy(model, Env(render=True), n_eval_episodes=10)
 
 print(f"mean_reward:{mean_reward:.2f}")
